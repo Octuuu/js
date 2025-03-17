b88d8e55ffa73d823b3ec9dadf0216588f402cf8
Octuuu <125304301+Octuuu@users.noreply.github.com>
2025-03-17 10:15:01 -0300

    dia1

---
+++ variables/variables.js
@@ -0,0 +1,6 @@
+var num  = 1 // declara una variable, inicializandolo opcionalmente a un valor. Podra cambiar su valor y su scope es local 
+
+let num = 1 // declara una variable local, en un bloque de ambito 
+
+const num = 1 // declara una varianle de solo lectura en un bloque de ambito, no podra cambiar de valor
+
